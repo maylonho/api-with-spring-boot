d9fc7b8975ccef85c7f61c29a11ae8de3b029ba1
maylonho <oliveira_maylon@hotmail.com>
2025-05-29 19:43:56 -0300

    created PersonController

---
+++ src/main/java/br/com/oliveira/controllers/PersonController.java
@@ -0,0 +1,25 @@
+package br.com.oliveira.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.oliveira.models.Person;
+import br.com.oliveira.services.PersonService;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonService service;
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+}
---
+++ src/main/java/br/com/oliveira/services/PersonService.java
@@ -0,0 +1,20 @@
+package br.com.oliveira.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.oliveira.models.Person;
+import br.com.oliveira.repositories.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public List<Person> findAll(){
+		return repository.findAll();
+	}
+}
